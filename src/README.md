34c2c9df63ebe53761f84467b45f35d778016031
“rikusv” <rikusv@gmail.com>
2018-09-24 17:50:49 +0200

    Separate input (src) from output files and document dev/build process

---
+++ src/README.md
@@ -0,0 +1,22 @@
+# Development Information
+
+The following documents the development environment setup and build process.
+
+## Setup
+
+```
+virtualenv src/env
+source src/env/bin/activate
+```
+
+Then in the activated environment:
+
+```
+pip install -r requirements.txt
+```
+
+## Build
+
+```
+python src/build.py
+```
---
+++ src/build.py
@@ -0,0 +1,22 @@
+import plot_interactive_geomap
+
+# Data files
+
+police = 'curated_data/points/police.csv')
+medical = 'curated_data/points/clinics.csv')
+legal = 'curated_data/points/points/courts.csv')
+shelters = 'curated_data/points/points/shelters_low_res.csv')
+crime_data = 'curated_data/points/incidence/1sexual_crimes_Incidence_sheltes_beds_per_province.csv')
+province = 'curated_data/points/province_shape/Province_New_SANeighbours.shp')
+
+# Output file
+
+outpath = './'
+name = 'index'
+
+
+def main():
+    plot_interactive_geomap.main(police, medical, legal, shelters, province, crime_data, outpath, name)
+
+if __name__ == '__main__':
+    main()
