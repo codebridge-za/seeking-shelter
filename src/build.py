b616e73c75fdb2d4d60e6da56600a8cb7be058b5
colin <colin.anthony001@gmail.com>
2019-08-02 14:15:16 +0200

    corrected path to shelters csv

--- src/build.py
+++ src/build.py
@@ -10,7 +10,7 @@ import codecs
 police = 'curated_data/points/police.csv'
 medical = 'curated_data/points/clinics.csv'
 legal = 'curated_data/points/courts.csv'
-shelters = 'curated_data/points/shelters_low_res.csv'
+shelters = 'curated_data/points/shelters.csv'
 crime_data = 'curated_data/incidence/1sexual_crimes_Incidence_sheltes_beds_per_province.csv'
 province = 'curated_data/province_shape/Province_New_SANeighbours.shp'
 
