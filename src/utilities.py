a82a1a4b36b926d70e634382050d176d22b1184f
colin <colin.anthony001@gmail.com>
2019-08-02 17:04:11 +0200

    fix telephone format error updated maps html files

--- src/utilities.py
+++ src/utilities.py
@@ -13,6 +13,9 @@ def parse_tel_numbers(raw_tel_text):
     separate_numbers = raw_tel_text.split(',') # "011222333, 011444444/5"
     for number_set in separate_numbers:
         numbers = number_set.split('/') # "011444444/5"
+        if len(numbers) == 1:
+            if len(numbers[0]) == 9:
+                numbers[0] = '0' + str(numbers[0])
         for i in range(1, len(numbers)):
             numbers[i] = numbers[0][:-len(numbers[i])] + numbers[i]
         final_numbers.extend(numbers)
